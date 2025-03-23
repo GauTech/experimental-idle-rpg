53fe426a56f2097ba2f40d8cc1db739b35dac4d4
GauTech <gregoryedwardsgau@gmail.com>
2025-03-23 15:16:02 +0000

    Update magic.js

--- src/magic.js
+++ src/magic.js
@@ -3,16 +3,17 @@
 import { character } from "./character.js";
 
 const magic_templates = {};
+const MagicSpell = {};
 
 
 
-(function(){
-    magic_templates["Strength buff"] = new MagicSpell({
+/*(function(){
+  //  magic_templates["Strength buff"] = new MagicSpell({
         name: "Strength buff", 
         description: "Strength buff",
 		mana_cost: 4,
         effects: [{effect: "Minor strength boost", duration: 300}],
     });
-})();
+})();*/
 
-export magic_templates, 
+export {magic_templates};
